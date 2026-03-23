bf5f01ba3bb07d5e5834e3e7ae63dce7643f37e1
Najla200 <najlahamza52@gmail.com>
2026-03-23 21:05:04 +0530

    added cors to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userRouter = require("./Route/UserRoute")
 const app = express();
 
 //Middleware 
+app.use(cors());
 app.use(express.json());
 app.use("/users",userRouter);
 
