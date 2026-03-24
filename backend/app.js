7b9ca6f7f456b2d72ee8147d6dbdf3ddbcfe6d37
Geethma Samarasinghe <geethsamarasinghe256@gmail.com>
2026-03-24 19:09:17 +0530

    Added model files for screens

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,20 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+
 const userRouter = require("./Route/UserRoute")
+const serviceHistoryRouter = require("./Route/ServiceHistoryRoute");
+const paymentHistoryRouter = require("./Route/PaymentHistoryRoute");
+const notificationRouter = require("./Route/NotificationRoute");
 const app = express();
 
 //Middleware 
 app.use(cors());
 app.use(express.json());
 app.use("/users",userRouter);
+app.use("/service-history", serviceHistoryRouter);
+app.use("/payment-history", paymentHistoryRouter);
+app.use("/notifications", notificationRouter);
 
 
 mongoose.connect(process.env.MONGO_URI)
