d781bb0856ec4b7c6773b66c02c00b9d59a54769
yuthila_banuka <banukadeseram12@gmail.com>
2026-03-28 09:38:27 +0530

    updated the user login part (check duplicate users / required fields )

--- backend/Model/UserModule.js
+++ backend/Model/UserModule.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 const userSchema = new Schema(
     {
         clerkId :{
@@ -45,5 +46,7 @@ const userSchema = new Schema(
 
     {timestamps : true }
 )
+
+
 
 module.exports = mongoose.model( "UserModel" , userSchema )
