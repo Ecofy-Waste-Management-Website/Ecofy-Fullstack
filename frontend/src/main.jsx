47c4f1e7632c22c4baaa00d2cbe299228f243f6b
Geethma Samarasinghe <geethsamarasinghe256@gmail.com>
2026-03-29 11:23:30 +0530

    Modified conflicts when merging

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,16 +3,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ClerkProvider } from '@clerk/react'
+import { BrowserRouter } from 'react-router-dom';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-import { BrowserRouter } from 'react-router-dom';
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider  publishableKey={PUBLISHABLE_KEY}>
       <App />
-    <ClerkProvider publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}>
       <BrowserRouter>
           <App />
       </BrowserRouter>
