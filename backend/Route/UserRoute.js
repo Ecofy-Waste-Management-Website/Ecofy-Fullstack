9205615de6aecd403d366a8f6fbe93d6c1e5e2a2
yuthila_banuka <banukadeseram12@gmail.com>
2026-03-29 09:22:47 +0530

    Implemted Role based Authentication

--- backend/Route/UserRoute.js
+++ backend/Route/UserRoute.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const {createUser} = require("../Controllers/UserControl")
+const { authorize } = require('../Middleware/roleCheck');
 
-// const {
-//   createUser,
-//   login,
-//   getAllUsers,
-//   getUserByClerkId,
-//   updateUser,
-//   updateUserStatus,
-//   deleteUser,
-// } = require("../Controllers/UserControl");
+
+const {
+  createUser,
+  login,
+  getAllUsers,
+  getUserByClerkId,
+  updateUser,
+  updateUserStatus,
+  deleteUser,
+} = require("../Controllers/UserControl");
 
 router.post("/signup", createUser);
+
+
+// router.post('/check-admin',checkAdmin);
 // router.post("/login", login);
 // router.get("/", getAllUsers);
 // router.get("/:clerkId", getUserByClerkId);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,42 @@
-import React, { useEffect, useState } from "react";
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Top-Header-Section/navbar/navbar'
-import ServiceHistory from './Components/Screens/ServiceHistory'
-import PaymentHistory from './Components/Screens/PaymentHistory'
-import Notifications from './Components/Screens/Notifications'
-import Footer from '../src/Components/Footer/footer'
-import Hero from '../src/Components/Hero-Section/Hero'
-
-
-
+import { Routes, Route , useLocation} from "react-router-dom";
+import ProtectedRoute from "./Components/Auth/ProtectedRoute";
+import adminDashboard from "./Components/Admin/adminDashboard";
 
 export default function App() {
-  // const [message, setMessage] = useState("");
-
-  // useEffect(() => {
-  //   fetch("http://localhost:5000/api/hello")
-  //     .then(res => res.json())
-  //     .then(data => setMessage(data.message))
-  //     .catch(err => console.error(err));
-  // }, []);
+  const location = useLocation();
+  const isAdminPage = location.pathname === "/admin";
 
   return (
+    // Use a Fragment <> instead of <Router>
+    <>
+      <AuthRedirect /> 
+      <main>
+        {/* Only show Navbar if we aren't on the admin page */}
+        {!isAdminPage && <Navbar />}
+        
+        <Routes>
+          <Route path="/" element={
+              <>
+                <Hero />
+                <Footer />
+              </>
+            } 
+          />
 
-    <BrowserRouter>
-      <Navbar/>
-      <Routes>
-        <Route path="/service-history" element={<ServiceHistory />} />
+          <Route path="/service-history" element={<ServiceHistory />} />
           <Route path="/payment-history" element={<PaymentHistory />} />
           <Route path="/notifications" element={<Notifications />} />
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
 
-
-  )
+          <Route 
+            path="/admin" 
+            element={
+              <ProtectedRoute allowedRoles={"Admin"}>
+                <adminDashboard/>
+              </ProtectedRoute>
+            } 
+          />
+        </Routes>
+      </main>
+    </>
+  );
 }
