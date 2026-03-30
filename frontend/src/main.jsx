2bf646735361bbd0a24102ab7e166a910b39a946
Geethma Samarasinghe <geethsamarasinghe256@gmail.com>
2026-03-30 12:04:43 +0530

    Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,14 +8,10 @@ import { BrowserRouter } from 'react-router-dom';
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider  publishableKey={PUBLISHABLE_KEY}>
       <App />
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
 );
