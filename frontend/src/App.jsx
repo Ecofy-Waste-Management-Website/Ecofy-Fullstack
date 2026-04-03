48b586dc303c81d544dcc6fabd03ae8cb61850b0
Geethma Samarasinghe <geethsamarasinghe256@gmail.com>
2026-04-03 10:48:16 +0530

    Added route protection for frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from 'react-router-dom';
+import {SignedIn, SignedOut, RedirectToSignIn} from '@clerk/clerk-react';
 import Navbar from './Components/Top-Header-Section/navbar/navbar'
 import ServiceHistory from './Components/Screens/ServiceHistory'
 import PaymentHistory from './Components/Screens/PaymentHistory'
@@ -9,16 +10,54 @@ import Footer from './Components/Footer/footer'
 import Hero from './Components/Hero-Section/Hero'
 import AdminDashboard from './Components/Admin/adminDashboard'
 
+
+function ProtectedRoute({children}) {
+  return (
+    <>
+    <SignedIn>{children}</SignedIn>
+    <SignedOut><RedirectToSignIn/></SignedOut>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<><Navbar/><Hero /><Footer/></>} />
-        <Route path="/dashboard" element={<><Navbar/><Dashboard /><Footer/></>} />
-        <Route path="/admin" element={<AdminDashboard />} />
-        <Route path="/service-history" element={<><Navbar/><ServiceHistory /><Footer/></>} />
-        <Route path="/payment-history" element={<><Navbar/><PaymentHistory /><Footer/></>} />
-        <Route path="/notifications" element={<><Navbar/><Notifications /><Footer/></>} />
+
+
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+          <Navbar /><Dashboard /><Footer />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/admin" element={
+          <ProtectedRoute>
+            <AdminDashboard />
+          </ProtectedRoute>
+        } />
+
+        
+        <Route path="/service-history" element={
+          <ProtectedRoute>
+            <Navbar /><ServiceHistory /><Footer />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/payment-history" element={
+          <ProtectedRoute>
+            <Navbar /><PaymentHistory /><Footer />
+          </ProtectedRoute>
+        } />
+
+        <Route path="/notifications" element={
+          <ProtectedRoute>
+            <Navbar /><Notifications /><Footer />
+          </ProtectedRoute>
+        } />
+        
       </Routes>
     </>
   )
