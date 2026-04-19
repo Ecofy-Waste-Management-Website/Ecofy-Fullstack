7d77bcdf2af5aafc6aaa4646fd90059e5f3b345e
Geethma Samarasinghe <geethsamarasinghe256@gmail.com>
2026-04-19 11:46:51 +0530

    Update

--- backend/Model/InquiryModel.js
+++ backend/Model/InquiryModel.js
@@ -48,33 +48,3 @@ const inquirySchema = new mongoose.Schema(
 
 module.exports = mongoose.model("Inquiry", inquirySchema);
 
-// const mongoose = require("mongoose");
-
-// const inquirySchema = new mongoose.Schema(
-//   {
-//     ticketNumber: {
-//       type: String,
-//       unique: true,
-//       required: true,
-//     },
-//     clerkId: {
-//       type: String,
-//       required: true,
-//       index: true,
-//     },
-//     name: { type: String, required: true },
-//     email: { type: String, required: true },
-//     phone: { type: String, required: true },
-//     category: { type: String, required: true },
-//     subject: { type: String, required: true },
-//     message: { type: String, required: true },
-//     status: {
-//       type: String,
-//       enum: ["Open", "In Progress", "Resolved", "Closed"],
-//       default: "Open",
-//     },
-//   },
-//   { timestamps: true }
-// );
-
-// module.exports = mongoose.model("Inquiry", inquirySchema);
