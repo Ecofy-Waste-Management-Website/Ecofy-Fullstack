bb9192ef81b627429f253854bf5b9ce9a428af93
Geethma Samarasinghe <geethsamarasinghe256@gmail.com>
2026-05-23 21:38:50 +0530

    Fixed admin notifications

--- backend/Model/NotificationModel.js
+++ backend/Model/NotificationModel.js
@@ -4,7 +4,8 @@ const notificationSchema = new mongoose.Schema(
   {
     clerkId: {
       type: String,
-      required: true,
+      required: false,
+      default:  "",
       index: true,
     },
     
